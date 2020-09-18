b9ff268fb5d5804678d66dcac972c571f95efedc
cabala <40265286+cabala3300@users.noreply.github.com>
2020-09-18 09:40:45 +0800

    11

---
+++ DownWell_Map/bin-debug/map/MapConfig.js
@@ -0,0 +1,12 @@
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var MapConfig = (function () {
+    function MapConfig() {
+    }
+    MapConfig._width = 70;
+    MapConfig._height = 70;
+    return MapConfig;
+}());
+__reflect(MapConfig.prototype, "MapConfig");
+//# sourceMappingURL=MapConfig.js.map
